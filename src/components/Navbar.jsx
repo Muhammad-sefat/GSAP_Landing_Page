5c6c43893319822e473025a6cbe3a65bf8a5e51f
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-07-14 23:48:37 +0600

    complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
   return (
     <nav>
-      <div>
-        <a href="#home">
-          <img src="" alt="image" />
+      <div className="px-5 lg:px-16">
+        <a className="flex gap-2 items-center" href="#home">
+          <img src="/images/logo.png" alt="image" />
           <p>Velvet Pour</p>
         </a>
         <ul>
