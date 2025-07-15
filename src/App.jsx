469967eba1113ecd7dcd0f45495badd004380ff1
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-07-16 00:27:13 +0600

    Complete full website

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
+      <Art />
+      <Menu />
     </main>
   );
 }
